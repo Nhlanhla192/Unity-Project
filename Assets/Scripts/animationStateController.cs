bbf8fc654db24c3430cc2efa6ab328a6ef278f78
Nhlanhla Mabasa <Mabasanhlanhla192@gmail.com>
2022-06-13 13:48:55 +0200

    First Commit

---
+++ Assets/Scripts/animationStateController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationStateController : MonoBehaviour {
+
+    Animator animator;
+
+	// Use this for initialization
+	void Start ()
+    {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        bool isWalking = animator.GetBool("isWalking");
+        bool forwardPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("r");
+
+        if (!isWalking && forwardPressed)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        if (isWalking && !forwardPressed)
+        {
+            animator.SetBool("isWalking", false);
+        }
+        if ( runPressed)
+        {
+            animator.SetBool("isRunning", true);
+        }
+        if ( !runPressed)
+        {
+            animator.SetBool("isRunning", false);
+        }
+
+    }
+}
